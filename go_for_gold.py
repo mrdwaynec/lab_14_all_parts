403f139010837814b6ee3ec5986d2cf9f57d28f8
Student <student@ent-v321-005.local>
2013-05-15 20:29:47 -0400

    lab 14

---
+++ go_for_gold.py
@@ -0,0 +1,22 @@
+"""
+go_for_gold.py
+=====
+Translate an athlete's finishing placement (1st, 2nd and 3rd) into its Olympic medal value: 1 for gold, 2 for silver, 3 for bronze and anything else means no medal.  Do this by asking for user input.  For example:
+
+What number should I translate into a medal?
+>1
+gold
+
+What number should I translate into a medal?
+>3
+bronze
+
+What number should I translate into a medal?
+>23
+no medal for you!
+
+"""
+medal = raw_input('what number should i translate into a medal? \n> ')
+
+if int(medal)2Z = 1:
+	print('gold')
